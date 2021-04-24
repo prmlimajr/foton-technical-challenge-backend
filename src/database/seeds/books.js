f1dbd962b3053492d6d3bf9a652d5bce5d2ba1a4
Paulo Lima <prmlimajr@hotmail.com>
2021-04-24 03:58:04 -0300

    created seeds

--- src/database/seeds/books.js
+++ src/database/seeds/books.js
@@ -5,9 +5,54 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex('books').insert([
-        { name: 'rowValue1', author: 'author', description: 'description' },
-        { name: 'rowValue2', author: 'author', description: 'description' },
-        { name: 'rowValue3', author: 'author', description: 'description' },
+        {
+          name: 'The Hill We Climb',
+          author: 'Amanda Gorman',
+          description:
+            "The poem read on President Joe Biden's Inauguration Day, by the youngest poet to write and perform an inaugural poem.",
+          cover:
+            'https://storage.googleapis.com/du-prd/books/images/9780593465271.jpg',
+        },
+        {
+          name: 'The Midnight Library',
+          author: 'Matt Haig',
+          description:
+            'Nora Seed finds a library beyond the edge of the universe that contains books with multiple possibilities of the lives one could have lived.',
+          cover:
+            'https://storage.googleapis.com/du-prd/books/images/9780525559474.jpg',
+        },
+        {
+          name: 'Good Company',
+          author: "Cynthia D'Aprix Sweeney",
+          description:
+            'The foundation of a marriage between actors is shaken when they reunite with an old friend who is now a TV star.',
+          cover:
+            'https://storage.googleapis.com/du-prd/books/images/9780062876003.jpg',
+        },
+        {
+          name: 'The Red Book',
+          author: 'James Patterson and David Ellis',
+          description:
+            'The second book in the Black Book thriller series. Chicago detective Billy Harney investigates his own past.',
+          cover:
+            'https://storage.googleapis.com/du-prd/books/images/9780316499408.jpg',
+        },
+        {
+          name: 'Northern Spy',
+          author: 'Flynn Berry',
+          description:
+            'The sister of a BBC producer may have joined the Irish Republican Army.',
+          cover:
+            'https://storage.googleapis.com/du-prd/books/images/9780735224995.jpg',
+        },
+        {
+          name: 'The Invisible Life of Addie Laure',
+          author: 'V.E. Schwab',
+          description:
+            'A Faustian bargain comes with a curse that affects the adventure Addie LaRue has across centuries.',
+          cover:
+            'https://storage.googleapis.com/du-prd/books/images/9780765387561.jpg',
+        },
       ]);
     });
 };
