323dcfed4ea7a600b6f26b6b94e5f1930c34992e
Paulo Lima <prmlimajr@hotmail.com>
2021-04-24 03:26:17 -0300

    database config

---
+++ src/database/connection.js
@@ -0,0 +1,22 @@
+require('dotenv/config');
+const knex = require('knex');
+const path = require('path');
+
+const connection = knex({
+  client: 'mysql2',
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+  migrations: {
+    directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
+  },
+  seeds: {
+    directory: path.resolve(__dirname, 'src', 'database', 'seeds'),
+  },
+  useNullAsDefault: true,
+});
+
+module.exports = connection;
